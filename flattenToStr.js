ba3650bf12cf8beba01010366efb8f2ed39c1c69
SmugBunnyDao <137497228+SmugBunnyDao@users.noreply.github.com>
2023-06-24 00:27:25 +0800

    Update flattenToStr.js

--- flattenToStr.js
+++ flattenToStr.js
@@ -1,8 +1,1 @@
-const fs = require("fs")
-// fs.writeFileSync("Flattened.sol", JSON.stringify(input))
-const contents = fs.readFileSync("./Flattened.sol", "utf8")
-
-
-const jsonString = JSON.stringify(contents)
-console.log('contents===>', jsonString)
 
